e2fdecb01669b2e050de7d7dbd54946d0670176e
Pascal Obiwumma <pobiwumma@gmail.com>
2025-03-06 00:34:04 +0100

    Wrote JavaScript form validation code

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-document.getElementById("myButton").addEventListener("click", (e) => {
-  console.log("Button was clicked!");
-  console.log(e); // Logs the full event object
-});
+const email = document.getElementById('email')
+const form = document.getElementById('form')
+const error = document.getElementById('error')
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const input = document.getElementsByClassName('input')
+console.log(input)
+console.log(input)
+form.addEventListener('submit', (e) => {
+  
+  if (email.value === '' || email.value == null) {
+    e.preventDefault()
+    error.innerText = 'Valid email required'
+    email.style.borderColor = 'red'
+    email.style.backgroundColor = '#ed626228'
+  }
+  if (!email.value.match(emailRegex)) {
+    e.preventDefault()
+    error.innerText = 'Valid email required'
+    email.style.border('red')
+  }
+})
+
+
+
+
+
+
+// document.getElementById("myButton").addEventListener("click", (e) => {
+//   console.log("Button was clicked!");
+//   console.log(e); // Logs the full event object
+// });
